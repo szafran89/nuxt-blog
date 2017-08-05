1686c7a312db95901815ea5903cd52f979027487
Pawel Szafranski <pawel.szafranski@snow.dog>
2017-08-06 01:47:03 +0200

    Added router base to logo

--- components/Nav.vue
+++ components/Nav.vue
@@ -3,7 +3,7 @@
     <nav class="nav">
       <div class="nav-left">
         <nuxt-link class="nav-item" to="/">
-          <img src="/bulma-logo.png" ale="Bulma logo">
+          <img src="/nuxt-blog/bulma-logo.png" ale="Bulma logo">
         </nuxt-link>
       </div>
       <div class="nav-center">
