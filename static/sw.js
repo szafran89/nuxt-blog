50367351e0ae2394af77418f304a0e38618fdff7
Pawel Szafranski <szafran89@gmail.com>
2017-10-20 01:43:42 +0200

    Updated static sw

--- static/sw.js
+++ static/sw.js
@@ -19,8 +19,8 @@ importScripts('workbox-sw.prod.v1.3.0.js');
  */
 const fileManifest = [
   {
-    "url": "/nuxt-blog/_nuxt/app.aad69f03263fea3f939c.js",
-    "revision": "9beef8843a0c1d988aa3ba98a9e055f1"
+    "url": "/nuxt-blog/_nuxt/app.7e5c5ec90b6c9f2c8d4c.js",
+    "revision": "c6428c46b58b4c7f8c02398c6b944561"
   },
   {
     "url": "/nuxt-blog/_nuxt/common.338ce5094a4b0096f65c.js",
@@ -31,8 +31,8 @@ const fileManifest = [
     "revision": "fe60b2f9d5c92bd637d6ec0801cd3e15"
   },
   {
-    "url": "/nuxt-blog/_nuxt/manifest.bfe5ed4742cf73f9d061.js",
-    "revision": "b4a27ade99bb3b23a29ddaaf60cfbeca"
+    "url": "/nuxt-blog/_nuxt/manifest.6d88a1710b7bf999b52e.js",
+    "revision": "deb2bd8bc5c154a9d48138510a9a771d"
   },
   {
     "url": "/nuxt-blog/_nuxt/pages/_slug.a9a12a0806acdabff31d.js",
