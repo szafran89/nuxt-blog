6a29a6a86db38caa130e8ce99525e3dc73cfc9f7
Pawel Szafranski <szafran89@gmail.com>
2017-10-20 01:40:39 +0200

    Added markdown module

--- static/sw.js
+++ static/sw.js
@@ -19,36 +19,36 @@ importScripts('workbox-sw.prod.v1.3.0.js');
  */
 const fileManifest = [
   {
-    "url": "/nuxt-blog/_nuxt/app.2161ac859c7f6318af07.js",
-    "revision": "6eee30a35989621692bdec8f6cfc6d16"
+    "url": "/nuxt-blog/_nuxt/app.aad69f03263fea3f939c.js",
+    "revision": "9beef8843a0c1d988aa3ba98a9e055f1"
   },
   {
-    "url": "/nuxt-blog/_nuxt/common.3ef32530518c0356975f.js",
-    "revision": "8c945294202e5335005ce4455c571574"
+    "url": "/nuxt-blog/_nuxt/common.338ce5094a4b0096f65c.js",
+    "revision": "2e17163c5305f37ae31c45b1388bbdf4"
   },
   {
-    "url": "/nuxt-blog/_nuxt/layouts/default.0b0804342c0e5a573fa6.js",
-    "revision": "f5d7ccd4c890ad33ec9fe021ac168dc1"
+    "url": "/nuxt-blog/_nuxt/layouts/default.cf6998bd1cf3fb361933.js",
+    "revision": "fe60b2f9d5c92bd637d6ec0801cd3e15"
   },
   {
-    "url": "/nuxt-blog/_nuxt/manifest.72cc09698c3dad6d3b0d.js",
-    "revision": "4d1b2b72532ee5e431264b41b5e56179"
+    "url": "/nuxt-blog/_nuxt/manifest.bfe5ed4742cf73f9d061.js",
+    "revision": "b4a27ade99bb3b23a29ddaaf60cfbeca"
   },
   {
-    "url": "/nuxt-blog/_nuxt/pages/_slug.e2aafed8a5e9e9937b45.js",
-    "revision": "214f24b9003c559153f00b58ae317d97"
+    "url": "/nuxt-blog/_nuxt/pages/_slug.a9a12a0806acdabff31d.js",
+    "revision": "cfb93f1abedd44b01a96165b44bf4fb2"
   },
   {
-    "url": "/nuxt-blog/_nuxt/pages/about.8ac09c2e178f5dda4ecd.js",
-    "revision": "4858393c903aab16eab0dbbb80ede6c0"
+    "url": "/nuxt-blog/_nuxt/pages/about.db552ef141bce1a3ebae.js",
+    "revision": "e5b6ee0796657bbb25a61e606c2ced5e"
   },
   {
-    "url": "/nuxt-blog/_nuxt/pages/category/_slug.4aa4b56831c85f09323a.js",
-    "revision": "44436da248fea6b0a801d1819be34641"
+    "url": "/nuxt-blog/_nuxt/pages/category/_slug.0a8cd337ca133eb41c09.js",
+    "revision": "88840b65b8048944c19fca4055c96132"
   },
   {
-    "url": "/nuxt-blog/_nuxt/pages/index.8824bac6b070a7e0152f.js",
-    "revision": "b19039ea119a2fea795e440fb02659af"
+    "url": "/nuxt-blog/_nuxt/pages/index.f03671d64ff184e5190e.js",
+    "revision": "fa976155cd2e3842f444363d6036acd4"
   }
 ];
 
