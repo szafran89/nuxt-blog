3c6ea0c2ed712bf936d05074fba5382c2f5c2762
Pawel Szafranski <szafran89@gmail.com>
2017-10-22 21:28:52 +0200

    Updated sw

--- static/sw.js
+++ static/sw.js
@@ -19,8 +19,8 @@ importScripts('workbox-sw.prod.v1.3.0.js');
  */
 const fileManifest = [
   {
-    "url": "/nuxt-blog/_nuxt/app.7e5c5ec90b6c9f2c8d4c.js",
-    "revision": "c6428c46b58b4c7f8c02398c6b944561"
+    "url": "/nuxt-blog/_nuxt/app.6724d89db6d4b0a871cb.js",
+    "revision": "528e1018583077e58191cd26379c7f92"
   },
   {
     "url": "/nuxt-blog/_nuxt/common.338ce5094a4b0096f65c.js",
@@ -31,24 +31,24 @@ const fileManifest = [
     "revision": "fe60b2f9d5c92bd637d6ec0801cd3e15"
   },
   {
-    "url": "/nuxt-blog/_nuxt/manifest.6d88a1710b7bf999b52e.js",
-    "revision": "deb2bd8bc5c154a9d48138510a9a771d"
+    "url": "/nuxt-blog/_nuxt/manifest.9686d903bf48e839e56a.js",
+    "revision": "b59ba206c3dc9aaa74de1da4e5681de7"
   },
   {
-    "url": "/nuxt-blog/_nuxt/pages/_slug.a9a12a0806acdabff31d.js",
-    "revision": "cfb93f1abedd44b01a96165b44bf4fb2"
+    "url": "/nuxt-blog/_nuxt/pages/_slug.b5f4a68f8acd8b19dfbb.js",
+    "revision": "30e88d293d79b2f4e78d97493c84b7f5"
   },
   {
     "url": "/nuxt-blog/_nuxt/pages/about.db552ef141bce1a3ebae.js",
     "revision": "e5b6ee0796657bbb25a61e606c2ced5e"
   },
   {
-    "url": "/nuxt-blog/_nuxt/pages/category/_slug.0a8cd337ca133eb41c09.js",
-    "revision": "88840b65b8048944c19fca4055c96132"
+    "url": "/nuxt-blog/_nuxt/pages/category/_slug.2aa1d74ebee0188ae746.js",
+    "revision": "26f0d91e86e97c7d29708395900367ce"
   },
   {
-    "url": "/nuxt-blog/_nuxt/pages/index.f03671d64ff184e5190e.js",
-    "revision": "fa976155cd2e3842f444363d6036acd4"
+    "url": "/nuxt-blog/_nuxt/pages/index.117feea3aff07815a161.js",
+    "revision": "06e2622ab2181911f0185ed995aacede"
   }
 ];
 
